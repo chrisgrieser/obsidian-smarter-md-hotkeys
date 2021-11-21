adfa00ebc0bdb3e089fd9f9aa8c5881ed5e973ea
Ross Keenan <rossk29@gmail.com>
2021-11-21 20:56:56 +0200

    refactor: :recycle: Lots of refactoring

--- main.ts
+++ main.ts
@@ -1,135 +1,110 @@
-import { MarkdownView, Plugin, EditorPosition, Editor } from "obsidian";
+import { COMMANDS } from "const";
+import { Editor, EditorPosition, Plugin } from "obsidian";
 
+// Override the obsidian module by adding the non-documented Editor methods
+// This makes it so that TypeScript stops shouting at you for accessing a property that it thinks doesn't exist
+// And gives you type-safety
+declare module "obsidian" {
+	interface Editor {
+		cm: {
+			state?: { wordAt: (offset: number) => EditorSelection };
+			findWordAt?: (pos: EditorPosition) => EditorSelection;
+		};
+	}
+}
 export default class SmarterMDhotkeys extends Plugin {
 	async onload() {
-		this.addCommand({
-			id: "smarter-asterisk-bold",
-			name: "Smarter Bold",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("**", "**", editor),
-		});
-		this.addCommand({
-			id: "smarter-underscore-bold",
-			name: "Smarter Underscore Bold",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("__", "__", editor),
-		});
-		this.addCommand({
-			id: "smarter-asterisk-italics",
-			name: "Smarter Italics",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("*", "*", editor),
-		});
-		this.addCommand({
-			id: "smarter-underscore-italics",
-			name: "Smarter Underscore Italics",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("_", "_", editor),
-		});
-		this.addCommand({
-			id: "smarter-comments",
-			name: "Smarter Comments",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("%%", "%%", editor),
-		});
-		this.addCommand({
-			id: "smarter-inline-code",
-			name: "Smarter Inline Code",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("`", "`", editor),
-		});
-		this.addCommand({
-			id: "smarter-highlight",
-			name: "Smarter Highlight",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("==", "==", editor),
-		});
-		this.addCommand({
-			id: "smarter-strikethrough",
-			name: "Smarter Strikethrough",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("~~", "~~", editor),
-		});
-		this.addCommand({
-			id: "smarter-wikilink",
-			name: "Smarter wikilink (internal link)",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("[[", "]]", editor),
-		});
-		this.addCommand({
-			id: "smarter-md-link",
-			name: "Smarter Markdown Link",
-			editorCallback: (editor: Editor, view: MarkdownView) => this
-				.expandAndWrap("[", "]()", editor),
+		COMMANDS.forEach((command) => {
+			const { id, name, before, after } = command;
+			this.addCommand({
+				id,
+				name,
+				editorCallback: (editor) =>
+					this.expandAndWrap(before, after, editor),
+			});
 		});
 
-		console.log ("Smarter MD Hotkeys loaded.");
+		console.log("Smarter MD Hotkeys loaded.");
 	}
 
 	async onunload() {
-		console.log ("Smarter MD Hotkeys unloaded.");
+		console.log("Smarter MD Hotkeys unloaded.");
 	}
 
 	expandAndWrap(beforeStr: string, afterStr: string, editor: Editor): void {
+		// Arrow functions are simpler
+		// No need to specify the return-type, Typescript can infer it
+		const offToPos = (offset: number) => editor.offsetToPos(offset);
 
-		function offToPos(offset: number): EditorPosition {
-			return editor.offsetToPos(offset);
-		}
-		function WordUnderCursor (ep: EditorPosition): Range{
+		// Return type is infered
+		function WordUnderCursor(ep: EditorPosition) {
 			// https://codemirror.net/doc/manual.html#api_selection
-			if (editor.cm?.findWordAt) return editor.cm.findWordAt(ep); //CM5
-			else if (editor.cm?.state.wordAt) return editor.cm.state.wordAt(editor.posToOffset(ep)); //CM6
+			if (editor.cm?.findWordAt) return editor.cm.findWordAt(ep);
+			//CM5
+			else if (editor.cm?.state.wordAt)
+				return editor.cm.state.wordAt(editor.posToOffset(ep)); //CM6
 		}
-		function trimSelection (trimBefArray: string[], trimAftArray: string[]): void{
+
+		function trimSelection(
+			trimBefArray: string[],
+			trimAftArray: string[]
+		): void {
 			let selection = editor.getSelection();
 			let sp = editor.posToOffset(editor.getCursor("from"));
 
 			//before
 			let trimFinished = false;
-			while (!trimFinished){
+			while (!trimFinished) {
 				let cleanCount = 0;
-				trimBefArray.forEach(str =>{
+				trimBefArray.forEach((str) => {
 					if (selection.startsWith(str)) {
-						selection = selection.slice (str.length);
+						selection = selection.slice(str.length);
 						sp += str.length;
 					} else cleanCount++;
 				});
-				if (cleanCount == trimBefArray.length || selection.length == 0) trimFinished = true;
+				// `a.length === 0` is the same as `!a.length`
+				if (cleanCount == trimBefArray.length || !selection.length)
+					trimFinished = true;
 			}
 
 			//after
 			trimFinished = false;
-			while (!trimFinished){
+			while (!trimFinished) {
 				let cleanCount = 0;
-				trimAftArray.forEach(str =>{
+				trimAftArray.forEach((str) => {
 					if (selection.endsWith(str)) {
-						selection = selection.slice (0, -str.length);
+						selection = selection.slice(0, -str.length);
 					} else cleanCount++;
 				});
-				if (cleanCount == trimAftArray.length || selection.length == 0) trimFinished = true;
+				if (cleanCount == trimAftArray.length || !selection.length)
+					trimFinished = true;
 			}
 
 			//block-ID
 			const blockID = selection.match(/ \^\w+$/);
-			if (blockID != null) selection = selection.slice (0, -blockID[0].length);
+			// `===` is safer than `==`
+			// https://stackoverflow.com/questions/359494/which-equals-operator-vs-should-be-used-in-javascript-comparisons
+			if (blockID !== null)
+				selection = selection.slice(0, -blockID[0].length);
 
-			if (selection.length == 0) {
+			if (!selection.length) {
 				console.log("only irrelevant characters where selected");
 				return;
 			}
 			editor.setSelection(offToPos(sp), offToPos(sp + selection.length));
 		}
 
-		const blen = beforeStr.length;
-		const alen = afterStr.length;
-		let wordExpanded = false;
-		let multiWordExpanded = false;
+		// I prefer defining similar variables inline using this syntax
+		const [blen, alen] = [beforeStr.length, afterStr.length];
+		let [wordExpanded, multiWordExpanded] = [false, false];
 
 		// Expand Selection to Word if there is no selection
-		const noSelPosition = editor.getCursor()
+		const noSelPosition = editor.getCursor();
 		if (!editor.somethingSelected()) {
-			const wordRange = WordUnderCursor (noSelPosition);
-			editor.setSelection(wordRange.anchor, wordRange.head);
+			// Object Destructuring is a good way to get the properties you need without having to define a temporary variable
+			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+			const { anchor, head } = WordUnderCursor(noSelPosition);
+			editor.setSelection(anchor, head);
 			wordExpanded = true;
 		}
 
@@ -139,86 +114,106 @@ export default class SmarterMDhotkeys extends Plugin {
 		trimSelection(trimBefore, trimAfter);
 
 		// Expand selection to word boundaries if multiple words
-		const selStart = editor.getCursor("from");
-		const selEnd = editor.getCursor("to");
+		const [selStart, selEnd] = [
+			editor.getCursor("from"),
+			editor.getCursor("to"),
+		];
 		const selected = editor.getSelection();
 
 		// Fix for punctuation messing up selection
-		if (selected.includes(" ")){
-			const firstWordRange = WordUnderCursor (selStart);
+		if (selected.includes(" ")) {
+			const firstWordRange = WordUnderCursor(selStart);
 			let lastWordRange;
 
 			//findatword reads to the right, so without "-1" the space would be read
-			if (selected.match(/[.,;:\-–—]$/) == null){
+			if (selected.match(/[.,;:\-–—]$/) == null) {
 				selEnd.ch--;
-				lastWordRange = WordUnderCursor (selEnd);
+				lastWordRange = WordUnderCursor(selEnd);
 				selEnd.ch++;
 			} else {
-				lastWordRange = WordUnderCursor (selEnd);
+				lastWordRange = WordUnderCursor(selEnd);
 			}
+			const { anchor, head } = lastWordRange;
 
 			// Fix for punctuation messing up selection
-			const lastWord = editor.getRange(lastWordRange.anchor, lastWordRange.head);
-			if (lastWord.match(/^[.,;:\-–—]/) != null){
-				lastWordRange.head.ch = lastWordRange.anchor.ch + 1;
+			const lastWord = editor.getRange(anchor, head);
+			// I think it's faster to `test` a regex than look for `match` if you only want to see if it matches
+			if (/^[.,;:\-–—]/.test(lastWord)) {
+				head.ch = anchor.ch + 1;
 			}
 
-			editor.setSelection(firstWordRange.anchor, lastWordRange.head);
+			editor.setSelection(firstWordRange.anchor, head);
 			multiWordExpanded = true;
 		}
 
 		// Get properties of selection
 		const selectedText = editor.getSelection();
-		const sp = editor.posToOffset(editor.getCursor("from")); // Starting position
+		// 'o' instead of 'p' because it's an offset, not a position
+		const so = editor.posToOffset(editor.getCursor("from")); // Starting offset
 		const len = selectedText.length;
-		const charsBefore = editor.getRange(offToPos(sp - blen), offToPos(sp));
-		const charsAfter = editor.getRange(offToPos(sp + len), offToPos(sp + len + alen));
-		const firstChars = editor.getRange(offToPos(sp), offToPos(sp + blen));
-		const lastChars = editor.getRange(offToPos(sp + len - alen), offToPos(sp + len));
+		const eo = so + len; // Ending offset
+
+		const [charsBefore, charsAfter] = [
+			editor.getRange(offToPos(so - blen), offToPos(so)),
+			editor.getRange(offToPos(eo), offToPos(eo + alen)),
+		];
+
+		const [firstChars, lastChars] = [
+			editor.getRange(offToPos(so), offToPos(so + blen)),
+			editor.getRange(offToPos(eo - alen), offToPos(eo)),
+		];
 
 		// Undo surrounding markup (outside selection)
-		if (charsBefore == beforeStr && charsAfter == afterStr) {
-			editor.setSelection(offToPos(sp - blen), offToPos(sp + len + alen));
+		if (charsBefore === beforeStr && charsAfter === afterStr) {
+			editor.setSelection(offToPos(so - blen), offToPos(eo + alen));
 			editor.replaceSelection(selectedText);
 			if (wordExpanded) {
+				// I've had issues when mutating properties of a `const` position before... I recommend making a copy of the position to use for this purpose only
 				noSelPosition.ch -= blen;
 				editor.setCursor(noSelPosition);
-			} else if (multiWordExpanded){
+			} else if (multiWordExpanded) {
+				// Same here. You might be introducing unintended side effects by mutating this object
 				selStart.ch -= blen;
 				selEnd.ch -= blen;
 				editor.setSelection(selStart, selEnd);
 			} else {
-				editor.setSelection(offToPos(sp - blen), offToPos(sp - blen + len));
+				editor.setSelection(
+					offToPos(so - blen),
+					offToPos(so - blen + len)
+				);
 			}
 
-		// Undo surrounding markup (inside selection)
+			// Undo surrounding markup (inside selection)
 		} else if (firstChars == beforeStr && lastChars == afterStr) {
-			editor.replaceSelection(selectedText.slice(blen,-alen));
-			if (wordExpanded) { //has to be considered, because findWordAt considers underscores as word-parts
+			editor.replaceSelection(selectedText.slice(blen, -alen));
+			if (wordExpanded) {
+				// Has to be considered, because findWordAt considers underscores as word-parts
 				noSelPosition.ch -= blen;
 				editor.setCursor(noSelPosition);
-			} else if (multiWordExpanded){
+			} else if (multiWordExpanded) {
 				selStart.ch -= blen;
 				selEnd.ch -= blen;
 				editor.setSelection(selStart, selEnd);
 			} else {
-				editor.setSelection(offToPos(sp), offToPos(sp + len - (blen + alen)));
+				editor.setSelection(offToPos(so), offToPos(eo - (blen + alen)));
 			}
 
-		// Do Markup
+			// Do Markup
 		} else {
 			editor.replaceSelection(beforeStr + selectedText + afterStr);
 			if (wordExpanded) {
 				noSelPosition.ch += blen;
 				editor.setCursor(noSelPosition);
-			} else if (multiWordExpanded){
+			} else if (multiWordExpanded) {
 				selStart.ch += blen;
 				selEnd.ch += blen;
 				editor.setSelection(selStart, selEnd);
 			} else {
-				editor.setSelection(offToPos(sp + blen), offToPos(sp + blen + len));
+				editor.setSelection(
+					offToPos(so + blen),
+					offToPos(so + blen + len)
+				);
 			}
 		}
-
 	}
 }

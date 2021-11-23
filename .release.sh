794152f511b6a0091a48c2540954df293b295468
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-11-23 22:26:48 +0100

    improved speed of manifest retrieval by using local manifest

--- .release.sh
+++ .release.sh
@@ -13,14 +13,13 @@ if [[ ! -f "./.github/workflows/release.yml" ]] ; then
 fi
 
 # get version number from the manifest of the latest release
-repoURL=$(git remote -v | grep git@github.com | grep fetch | head -n1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/https:\/\//' -e 's/\.git//' )
-manifestURL="$repoURL"/releases/latest/download/manifest.json
-lastVersion=$(curl -sL "$manifestURL" | grep "version" | cut -d\" -f4)
+lastVersion=$(cat "./manifest.json" | grep "version" | cut -d\" -f4)
 echo "last version: $lastVersion"
 
 # Ask for new version number
 echo -n "next version: "
 read nextVersion
+echo ""
 
 # set version number in `manifest.json`
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "manifest.json"

1f2cc1a930c122de6f72efd32f9293880fd26abe
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-15 18:03:18 +0100

    shellcheck (linting)

--- .release.sh
+++ .release.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-
 # Release Obsidian Plugin
 # https://forum.obsidian.md/t/using-github-actions-to-release-plugins/7877
 # https://marcus.se.net/obsidian-plugin-docs/publishing/release-your-plugin-with-github-actions
@@ -19,32 +18,34 @@ if [[ ! -f "./.github/workflows/release.yml" ]] ; then
 fi
 
 # Lint
-cd "$(dirname "$0")"
-eslint --fix *.ts
-markdownlint --fix *.md
+cd "$(dirname "$0")" || exit
+eslint --fix ./*.ts
+# disable strong style, since the variation is needed for examples in the table
+markdownlint --fix --diable strong-style ./*.md
 
 # get version number from the manifest of the latest release
-lastVersion=$(cat "./manifest.json" | grep "version" | cut -d\" -f4)
+lastVersion=$(grep "version" "./manifest.json" | cut -d\" -f4)
 echo "last version: $lastVersion"
 
 # Ask for new version number
 echo -n "next version: "
-read nextVersion
+read -r nextVersion
 echo ""
 
 # set version number in `manifest.json`
 sed -E -i '' "s/\"version\".*/\"version\": \"$nextVersion\",/" "manifest.json"
 
 # add version number in `versions.json`, assuming same compatibility
-cat "versions.json" | egrep -v "^$" | grep -v "}" | sed -e '$ d' > temp
-minObsidianVersion=$(cat "versions.json" | egrep -v "^$" | grep -v "}" | tail -n1 | cut -d\" -f4)
+grep -Ev "^$" "versions.json" | grep -v "}" | sed -e '$ d' > temp
+minObsidianVersion=$(grep -Ev "^$" "versions.json" | grep -v "}" | tail -n1 | cut -d\" -f4)
+# shellcheck disable=SC2129
 echo "  \"$lastVersion\": \"$minObsidianVersion\"," >> temp
 echo "  \"$nextVersion\": \"$minObsidianVersion\"" >> temp
 echo "}" >> temp
 mv temp versions.json
 
 # update changelog
-echo "- "$(date +"%Y-%m-%d")"	release $nextVersion" > ./Changelog.md
+echo "- ""$(date +"%Y-%m-%d")""	release $nextVersion" > ./Changelog.md
 git log --pretty=format:"- %ad%x09%s" --date=short | grep -Ev "minor$" | grep -Ev "patch$" | grep -Ev "typos?$" | grep -v "refactoring" | grep -v "Add files via upload" | grep -Ev "\tDelete" | grep -Ev "\tUpdate.*\.md" | sed -E "s/\t\+ /\t/g" >> ./Changelog.md
 
 # push the manifest and versions JSONs

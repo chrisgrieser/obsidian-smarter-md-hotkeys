33d7e9f9e35420fcc40dd46f7ecd0ec717c4ff07
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-07 21:03:11 +0200

    patch

--- main.ts
+++ main.ts
@@ -115,17 +115,17 @@ export default class SmarterMDhotkeys extends Plugin {
 		// Hide Notice
 		} else if (commandID === "hide-notice") {
 
-			const isVersionFifteen = requireApiVersion("0.15");
+			const isVersionFifteen = requireApiVersion("0.15.0");
 			if (isVersionFifteen) {
 				// @ts-ignore
-				for (const el of activeDocument.body.getElementsByClassName("notice")) 
+				for (const el of activeDocument.body.getElementsByClassName("notice"))
 					el.hide();
-				
+
 			} else {
 				// @ts-ignore
-				for (const el of document.body.getElementsByClassName("notice")) 
+				for (const el of document.body.getElementsByClassName("notice"))
 					el.hide();
-				
+
 			}
 			const optionEnabled = this.app.vault.getConfig("readableLineLength");
 			this.app.vault.setConfig("readableLineLength", !optionEnabled);
